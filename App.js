7a3cb491c24da8726f75b60e22e6874e21b70153
8Grom8 <80001894+8Grom8@users.noreply.github.com>
2022-05-10 23:51:23 +0300

    xvatit poka)

--- App.js
+++ App.js
@@ -3,12 +3,7 @@ import { StyleSheet, View, FlatList} from 'react-native';
 import Form from './components/Form';
 import Header from './components/Header';
 import List from './components/List';
-import LoginScreen from './components/screens/auth/loginScreen';
-import RegistrationScreen from './components/screens/auth/registarationScreen';
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
 
-const AuthStack = createStackNavigator();
 
 export default function App() {
 
@@ -26,24 +21,16 @@ export default function App() {
     });
   }
 
-  // return (
-  //   <View>
-  //     <Header />
-  //     <Form addHandler={addHandler}/>
-  //     <View>
-  //       <FlatList data={listOfItems} renderItem={({item}) => (
-  //       <List e={item} deleteHandler={deleteHandler}
-  //       />)} />
-  //     </View>
-  //   </View>
-  // );
   return (
-    <NavigationContainer>
-     <AuthStack.Navigator>
-      <AuthStack.Screen name="Register" component={RegisterScreen} />
-      <AuthStack.Screen name="Login" component={LoginScreen} />
-     </AuthStack.Navigator>
-    </NavigationContainer>
+    <View>
+      <Header />
+      <Form addHandler={addHandler}/>
+      <View>
+        <FlatList data={listOfItems} renderItem={({item}) => (
+        <List e={item} deleteHandler={deleteHandler}
+        />)} />
+      </View>
+    </View>
   );
 }
 
